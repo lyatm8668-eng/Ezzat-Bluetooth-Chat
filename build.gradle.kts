e97fa7186be16cba3f51cab6a92b2bd5ff62f0ca
lyatm8668-eng <lyatm8668@gmail.com>
2026-08-13 01:54:08 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "com.azzat.bluetoothchat"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.azzat.bluetoothchat"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.compose.ui:ui:1.7.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
+    implementation("androidx.compose.material3:material3:1.3.1")
 }
