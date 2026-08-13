6b36717fefeb2dbfce8edbf795064204e58909fc
lyatm8668-eng <lyatm8668@gmail.com>
2026-08-13 03:16:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
     namespace = "com.azzat.bluetoothchat"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.azzat.bluetoothchat"
         minSdk = 26
@@ -13,7 +15,17 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
